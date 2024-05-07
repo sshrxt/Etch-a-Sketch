8cfba20a230d35aa0b46f7cce4b33ffa62d97ca8
Shrut <shrutpatel20@gmail.com>
2024-05-07 13:06:04 -0500

    add reset and color functionalities

--- script.js
+++ script.js
@@ -2,12 +2,34 @@ let gridSize = 600;
 let row = 16;
 let col = 16;
 
+let color = "black";
+
 const grid = document.querySelector(".sketch-container");
 grid.style.width = `${gridSize}px`;
 grid.style.height = `${gridSize}px`;
 
 function changeCellColor() {
-  this.style.backgroundColor = "black";
+  this.style.backgroundColor = getCellColor();
+}
+
+function getCellColor() {
+  if (color === "black") {
+    return "black";
+  } else {
+    return getRandomColor();
+  }
+}
+
+function getRandomColor() {
+  
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256); 
+  const blue = Math.floor(Math.random() * 256); 
+
+  // Construct the CSS color string using RGB values
+  const color = `rgb(${red}, ${green}, ${blue})`;
+
+  return color;
 }
 
 function createGrid() {
@@ -21,6 +43,27 @@ function createGrid() {
 
     gridCell.addEventListener("mouseover", changeCellColor);
   }
+  addEventListener();
+}
+
+function addEventListener() {
+  const random = document.querySelector("#random");
+  const black = document.querySelector("#black");
+  const reset = document.querySelector("#reset");
+
+  random.addEventListener("click", () => {
+    color = "change";
+  });
+  black.addEventListener("click", () => {
+    color = "black";
+  });
+  reset.addEventListener("click", ()=> {
+    const grid = document.querySelectorAll('.sketch-container div');
+    for(let i = 0; i < grid.length; i++) {
+        const currDiv = grid[i];
+        currDiv.style.backgroundColor = "white";
+    }
+  });
 }
 
 createGrid();
